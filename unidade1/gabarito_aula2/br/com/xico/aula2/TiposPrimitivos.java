12e50bfbd22cbff419138b7f71b63db7bd553bd0
Juan2300 <juancarlosfd230105@gmail.com>
2024-07-19 21:29:09 -0300

    Add files via upload

---
+++ unidade1/gabarito_aula2/br/com/xico/aula2/TiposPrimitivos.java
@@ -0,0 +1,43 @@
+//fully qualified name
+package br.com.xico.aula2;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //Crie um programa que declare variáveis de todos os tipos primitivos em Java 
+        //(byte, short, int, long, float, double, boolean, char)
+        // e uma variável do tipo String. 
+        //Atribua valores a elas e imprima esses valores no console.
+
+        byte codigoByte = 112;
+        System.out.println("");
+        System.out.println(codigoByte);
+        
+        short codigoShort = 244;
+        System.out.println("");
+        System.out.println(codigoShort);
+        
+        int codigoInt = 303;
+        System.out.println("");
+        System.out.println(codigoInt);
+
+        long codigoLong = 456;
+        System.out.println("");
+        System.out.println(codigoLong);
+
+        float codigoFloat = 512;
+        System.out.println("");
+        System.out.println(codigoFloat);
+
+        double codigoDouble = 640;
+        System.out.println("");
+        System.out.println(codigoDouble);
+
+        boolean codigoBoolean = false;
+        System.out.println("");
+        System.out.println(codigoBoolean);
+
+        char codigoChar = 864;
+        System.out.println("");
+        System.out.println(codigoChar);
+    }
+}
