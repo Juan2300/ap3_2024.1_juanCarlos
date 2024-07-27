e248be3cfc77f243fc24fc4a853bf4847824a414
Juan2300 <juancarlosfd230105@gmail.com>
2024-07-26 21:11:02 -0300

    Add files via upload

---
+++ unidade1/gabarito_aula3/MediaProgEstruturada.java
@@ -0,0 +1,14 @@
+package unidade1.gabarito_aula3;
+
+public class MediaProgEstruturada {
+
+    public static void main(String[] args) {
+
+        double nota1 = 7.0;
+        double nota2 = 8.0;
+        double nota3 = 6.5;
+        double soma = nota1 + nota2 + nota3;
+        double media = soma / 3;
+        System.out.println(media);
+        
+}
