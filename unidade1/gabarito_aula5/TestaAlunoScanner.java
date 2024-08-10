a007b8a2983710dd78283785507d30db4213df82
Juan2300 <juancarlosfd230105@gmail.com>
2024-08-09 21:08:40 -0300

    Add files via upload

---
+++ unidade1/gabarito_aula5/TestaAlunoScanner.java
@@ -0,0 +1,25 @@
+package br.com.xico.unidade1.gabarito_aula5;
+
+import java.util.Scanner;
+
+import br.com.xico.unidade1.gabarito_aula4.Aluno;
+
+public class TestaAlunoScanner {
+    public static void main(String[] args) {
+        Aluno umAluno = new Aluno();
+        Scanner leitor = new Scanner(system.in);
+        System.out.println();
+
+        System.out.println("Digite o nome do aluno");
+        string nomeDoAluno = teclado.nextline();
+        System.out.println("Digite a nota 1: ");
+        double nota1 = teclado.nextDouble();
+        System.out.println("Digite a nota 2: ");
+        double nota2 = teclado.nextDouble();
+        System.out.println("Digite a nota 3: ");
+        double nota3 = teclado.nextDouble();
+        
+        
+    }
+
+}
