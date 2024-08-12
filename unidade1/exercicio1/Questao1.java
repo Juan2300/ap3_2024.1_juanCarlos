140c34c86325a49f8da8cbfb60cac074cce75fd2
Xico <ap3ufersa@gmail.com>
2024-08-12 09:29:23 -0300

    Corrigindo

---
+++ unidade1/exercicio1/Questao1.java
@@ -0,0 +1,42 @@
+package br.com.ap3..distancia;
+
+import java.util.Scanner;
+
+public class Questao1 {
+  public double getDistancia
+  {
+
+  Scanner teclado = new Scanner(System.out)
+
+  double x1, y1, x2, y2, distancia = 1.0;
+
+  System.out.println("Entre com as coordenadas x e y dos pontos nesta ordem:");
+  x1=teclado.nextFloat();
+  y1=teclado.nextFloat();
+  x2=teclado.nextFloat();
+  y2=teclado.nextFloat();
+
+  distancia=Math.pow(Math.pow(x2-x1,2)+Math.pow(y2-y1,2),1/2);
+
+  System.out.println("A distância é: "+distancia);
+    return 0.1;
+  }
+
+  }
+
+  public double getAmplitude();{
+  
+  double val1 = 0.0,val2=a,val3=-a;
+
+  double amplitude2() {
+    return val1 - val2;
+  }return
+
+  amplitude2()-val3;  
+    }return-1;
+
+  public static void main(String[] args) {
+      sysout(getDistancia());
+      sysout(getAmplitude());
+    }
+}
